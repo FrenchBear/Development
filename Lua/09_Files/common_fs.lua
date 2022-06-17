6ffc915b7c27479be7905a7832a5e1518f342ac1
French Bear <FrenchBear38@outlook.com>
2022-06-17 11:11:05 +0200

    options processing in Lua

--- Lua/09_Files/common_fs.lua
+++ Lua/09_Files/common_fs.lua
@@ -3,6 +3,7 @@
 --
 -- 2022-06-16   PV
 
+-- Check if a file exists
 function File_Exists(filename)
     local f = io.open(filename)
     if f then
@@ -11,4 +12,84 @@ function File_Exists(filename)
     else
         return false
     end
+end
+
+-- Helper, returns last position of pat (plain string) in s, or nil if not found
+local function instrrev(s, pat)
+    local lp = 0
+    while true do
+        local p = string.find(s, pat, lp + 1, true)
+        if p == nil then
+            return lp > 0 and lp or nil
+        end
+        lp = p
+    end
+end
+
+-- Retourne un nom de fichier sans son chemin ni son disque
+function FilePart(fullpath)
+    local pb1 = instrrev(fullpath, [[\]]) or 0
+    local pb2 = instrrev(fullpath, [[/]]) or 0
+    local pb3 = instrrev(fullpath, [[:]]) or 0
+    local pb = math.max(pb1, pb2, pb3)
+    if pb == 0 then
+        return fullpath
+    else
+        return string.sub(fullpath, pb + 1)
+    end
+end
+
+-- Retourne le chemin d'un fichier sans son fichier.  Ne se termine pas par \ sauf si le fichier est à la racine.
+-- Retourne une chaîne vide s'il n'y a pas de dossier.
+function FolderPart(fullpath)
+    local pb1 = instrrev(fullpath, [[\]]) or 0
+    local pb2 = instrrev(fullpath, [[/]]) or 0
+    local pb3 = instrrev(fullpath, [[:]]) or 0
+    local pb = math.max(pb1, pb2, pb3 + 1)
+    if pb3 == 2 and math.max(pb1, pb2) == 3 then pb = pb + 1 end -- C:\File... returns C:\, that's an exception
+    if pb3 == 0 and math.max(pb1, pb2) == 1 then pb = pb + 1 end -- \File... returns \, that's another exception
+    if pb == 0 then
+        return ""
+    else
+        return string.sub(fullpath, 1, pb - 1)
+    end
+end
+
+-- Retourne le chemin d'un fichier sans extension.
+-- Les éventuels répertoires en tête sont conservés dans le résultat.
+function Basename(fullpath)
+    local pb1 = instrrev(fullpath, [[\]]) or 0
+    local pb2 = instrrev(fullpath, [[/]]) or 0
+    local pb3 = instrrev(fullpath, [[:]]) or 0
+    local pp = instrrev(fullpath, [[.]]) or 0
+    local pb = math.max(pb1, pb2, pb3)
+    if pp < pb then
+        return fullpath
+    else
+        return string.sub(fullpath, 1, pp - 1)
+    end
+end
+
+-- Retourne juste les fichiers d'un dossier, noms.ext sans chemins
+function GetFiles(source)
+    local t = {}
+    local pfile = io.popen('dir /b /a-d "' .. source .. '"')
+    if pfile == nil then error("popen dir " .. source .. " failed") end
+    for filename in pfile:lines() do
+        t[#t+1] = filename
+    end
+    pfile:close()
+    return t
+end
+
+-- Retourne juste les sous-dossiers d'un dossier, noms sans chemins
+function GetFolders(source)
+    local t = {}
+    local pfile = io.popen('dir /b /ad "' .. source .. '"')
+    if pfile == nil then error("popen dir " .. source .. " failed") end
+    for filename in pfile:lines() do
+        t[#t+1] = filename
+    end
+    pfile:close()
+    return t
 end
---
+++ Lua/10_Args/args.lua
@@ -0,0 +1,69 @@
+-- arg.lua
+-- Learning lua, decode arg options
+-- Start with completely manual code, mays switch to a getopt function later
+--
+-- 2022-06-17   PV
+
+-- For VSCode, debug terminal is not utf8 by default
+os.execute("chcp 65001 >NUL")
+
+-- Simulation, overrides arg for this example
+local arg = {}
+arg[0] = "script name"
+arg[1] = "-r"
+arg[2] = "15"
+arg[3] = "-v"
+arg[4] = "file1"
+arg[5] = "file2"
+
+local rows = nil
+local cols = nil
+local verbose = false
+local files = {}
+
+-- Use a while and not a for since we may increase loop variable inside the loop
+local i = 1
+while i <= #arg do
+    local a = arg[i]:lower()
+    if i == 1 and a == "?" then a = "-?" end
+    if string.sub(a, 1, 1) == "-" then
+        if a == "-r" then
+            i = i + 1
+            rows = tonumber(arg[i])
+            if rows == nil or rows < 5 or rows > 200 then
+                error("Invalid option -r value " .. arg[i] .. ", must be an integer between 5 and 200")
+            end
+        elseif a == "-c" then
+            i = i + 1
+            cols = tonumber(arg[i])
+            if cols == nil or cols < 5 or cols > 200 then
+                error("Invalid option -c value " .. arg[i] .. ", must be an integer between 5 and 200")
+            end
+        elseif a == "-v" then
+            verbose = true
+        elseif a == "-?" then
+            print([[Generation of a random labyrinth, and optionally show solution path (Lua version)
+Usage: script [-r rows] [-c cols] [-v] file...
+-h           Shows this message
+-a           Simple print (ASCII only)
+-b a|s|b|d|r Border style, a=ASCII, s=Simple, b=Bold, d=Double, r=Rounded (use chcp 65001 for styles s/b/d/r on Windows)
+-s           Shows solution
+-m           Monochrome (no color) solution output
+-d           Shuffle random generator
+-r rows      Number of rows in [5..200]
+-c cols      Number of columns in [5..200] ]])
+            return
+        else
+            error("Invalid/unsupported option " .. arg[i])
+        end
+    else
+        -- Non-option arguments
+        files[#files + 1] = arg[i] -- preserve case
+    end
+    i = i + 1
+end
+
+print("rows", rows)
+print("cols", cols)
+print("verbose", verbose)
+print("files", table.concat(files, ", "))
