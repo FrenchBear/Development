bb5af37ea8887dec7bc3508f17991684705d3ad8
French Bear <FrenchBear38@outlook.com>
2022-07-04 23:23:57 +0200

    Lua deque

---
+++ Lua/14_Collections/deque.lua
@@ -0,0 +1,82 @@
+-- deque.lua
+-- Learning lua, classical structures
+-- Implementation of a deque as a module
+--
+-- 2022-07-04   PV
+
+Deque = {
+    __name = "Deque",
+
+    new = function(self, object)
+        object = object or {}
+        setmetatable(object, self)
+        self.__index = self
+
+        self:clear()
+
+        return object
+    end,
+
+    clear = function(self)
+        self.first = 0
+        self.last = -1
+        self.list = {}
+    end,
+
+    pushFirst = function(self, value)
+        self.first = self.first - 1
+        self.list[self.first] = value
+    end,
+
+    pushLast = function(self, value)
+        self.last = self.last + 1
+        self.list[self.last] = value
+    end,
+
+    popFirst = function(self)
+        if self.first > self.last then error("list is empty") end
+        local value = self.list[self.first]
+        self.list[self.first] = nil -- to allow garbage collection
+        self.first = self.first + 1
+        return value
+    end,
+
+    popLast = function(self)
+        if self.first > self.last then error("list is empty") end
+        local value = self.list[self.last]
+        self.list[self.last] = nil -- to allow garbage collection
+        self.last = self.last - 1
+        return value
+    end,
+
+    __tostring = function(self)
+        local t = {}
+        for i = self.first, self.last do
+            t[#t + 1] = string.format("%q", self.list[i])
+        end
+        return self.__name .. " [" .. table.concat(t, ", ") .. "]"
+    end,
+
+    __len = function(self)
+        return self.last - self.first + 1
+    end,
+}
+
+-- Tests
+if not pcall(debug.getlocal, 4, 1) then -- https://stackoverflow.com/questions/49375638/how-to-determine-whether-my-code-is-running-in-a-lua-module
+    -- For VSCode, debug terminal is not utf8 by default
+    os.execute("chcp 65001 >NUL")
+
+    local d = Deque:new()
+    d:pushFirst(1)
+    d:pushFirst(2)
+    d:pushFirst(3)
+    d:pushLast('A')
+    d:pushLast('B')
+    d:pushLast('C')
+
+    print(d, "len = " .. #d)
+    print("d:popFirst()", d:popFirst())
+    print("d:popLast()", d:popLast())
+    print(d, "len = " .. #d)
+end
