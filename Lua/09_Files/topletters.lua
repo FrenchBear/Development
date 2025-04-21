f5ac5be43c1dabb4fa9c8f1bfd3ffdac8f02ba2b
French Bear <FrenchBear38@outlook.com>
2025-04-21 12:56:32 +0200

    Save

--- Lua/09_Files/topletters.lua
+++ Lua/09_Files/topletters.lua
@@ -2,7 +2,8 @@
 -- Play with files in Lua, find the most common letters and pairs
 --
 -- 2022-06-16   PV
--- 2077-07-21	PV		Use <close> on file variable
+-- 2022-07-21	PV		Use <close> on file variable
+
 -- For VSCode, debug terminal is not utf8 by default
 if package.config:sub(1,1)=='\\' then os.execute("chcp 65001 >NUL") end
 
