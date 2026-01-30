cbe0cc652232b1aa1fde80471776207fb1867637
French Bear <frenchbear38@outlook.com>
2026-01-30 13:03:08 +0100

    Prepare Egyptian Fractions in HTML+JS

---
+++ JavaScript/04 EgyptianFractions/fraction.js
@@ -0,0 +1,12 @@
+class Fraction {
+    constructor(num, den) {
+        this.Num = num;
+        this.Den = den;
+    }
+}
+
+// let f = new Fraction(8,11);
+// console.log(f);
+// 
+// let l = [new Fraction(1,2), new Fraction(1,5), new Fraction(1,37), new Fraction(1,4070)];
+// console.log(l);
