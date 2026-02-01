8be10c4b0cb587fc7940cc58b208ddac579efe0c
French Bear <frenchbear38@outlook.com>
2026-02-01 11:18:14 +0100

    Finished Egyptian fractions in HTML+CSS+JavaScript

--- JavaScript/04 EgyptianFractions/fraction.js
+++ JavaScript/04 EgyptianFractions/fraction.js
@@ -1,12 +1,38 @@
+// Basic implementation of a fraction in JavaScript
+//
+// 2026-02-01 	PV
+
 class Fraction {
-    constructor(num, den) {
-        this.Num = num;
-        this.Den = den;
+constructor(num, den) {
+        this.num = num;
+        this.den = den;
+    }
+
+    // Static helper to find GCD
+    static gcd(a, b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b) {
+            a %= b;
+            [a, b] = [b, a];
+        }
+        return a;
+    }
+
+    // Method to return a new, simplified Fraction
+    simplify() {
+        const commonDivisor = Fraction.gcd(this.num, this.den);
+        return new Fraction(this.num / commonDivisor, this.den / commonDivisor);
+    }
+
+    // Subtract and auto-simplify
+    subtract(other) {
+        const newNum = this.num * other.den - other.num * this.den;
+        const newDen = this.den * other.den;
+        return new Fraction(newNum, newDen).simplify();
+    }
+
+    toString() {
+        return `${this.num}/${this.den}`;
     }
 }
-
-// let f = new Fraction(8,11);
-// console.log(f);
-// 
-// let l = [new Fraction(1,2), new Fraction(1,5), new Fraction(1,37), new Fraction(1,4070)];
-// console.log(l);
