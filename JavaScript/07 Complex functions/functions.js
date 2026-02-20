17b9b654865d65517887ac3d39dc832ea826fe6d
French Bear <frenchbear38@outlook.com>
2026-02-20 13:56:59 +0100

    Complex functions in HTML/JavaScript

--- JavaScript/07 Complex functions/functions.js
+++ JavaScript/07 Complex functions/functions.js
@@ -41,7 +41,7 @@ class Complex {
         const product = logZ.mul(n);
         return product.exp();
     }
-    
+
     // e^z
     exp() {
         const expRe = Math.exp(this.re);
@@ -62,12 +62,36 @@ class Complex {
     }
 }
 
-// Collection of functions to be plotted
+// Collection (map) of functions to be plotted
 const complexFunctions = {
-    f_z_pow_1_5: (z) => z.pow(new Complex(1.5, 0)),
-    f_1_div_z: (z) => new Complex(1, 0).div(z),
-    f_z_pow_a_bi: (z, a, b) => z.pow(new Complex(a, b)),
-    f_b_times_z_pow_a: (z, a, b) => new Complex(b,0).mul(z.pow(new Complex(a, 0))),
-    f_exp_z: (z) => z.exp(),
-    f_log_z: (z) => z.log(),
+    f_z_pow_1_5: {
+        func: (z) => z.pow(new Complex(1.5, 0)),
+        usesA: false,
+        usesB: false
+    },
+    f_1_div_z: {
+        func: (z) => new Complex(1, 0).div(z),
+        usesA: false,
+        usesB: false
+    },
+    f_z_pow_a_bi: {
+        func: (z, a, b) => z.pow(new Complex(a, b)),
+        usesA: true,
+        usesB: true
+    },
+    f_a_times_z_pow_b: {
+        func: (z, a, b) => new Complex(a, 0).mul(z.pow(new Complex(b, 0))),
+        usesA: true,
+        usesB: true
+    },
+    f_a_times_exp_z: {
+        func: (z, a) => new Complex(a, 0).mul(z.exp()),
+        usesA: true,
+        usesB: false
+    },
+    f_log_z: {
+        func: (z) => z.log(),
+        usesA: false,
+        usesB: false
+    },
 };
